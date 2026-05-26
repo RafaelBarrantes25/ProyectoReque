2f979979ffd2b1902afb9605524281489e058cdc
WolfdryP <deryanfallas@gmail.com>
2026-05-25 18:07:09 -0600

    Index

--- index.php
+++ index.php
@@ -1,52 +1,107 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 
 <head>
-    <title>AvisaTEC</title>
+
+    <meta charset="UTF-8">
+
+    <title>
+
+        AvisaTEC | Iniciar sesión
+
+    </title>
+
+    <link
+    rel="stylesheet"
+    href="css/registrar.css">
+
 </head>
 
 <body>
 
-    <img
-    src="imagenes/logoTecAzul.webp"
-    alt="Logo del Instituto Tecnológico de Costa Rica"
-    height="65">
+    <header class="barra-logo">
 
-    <h1>Iniciar sesión</h1>
+        <img
+        src="imagenes/logotecblanco.svg"
+        alt="Logo del Instituto Tecnológico de Costa Rica"
+        height="65">
 
-    <form action="includes/login.php" method="POST">
+    </header>
 
-        <label>Correo:</label>
+    <div class="barra-horizontal-roja"></div>
+
+    <h1 class="titulo">
+
+        Iniciar sesión
+
+    </h1>
+
+    <br><br>
+
+    <form
+    action="includes/login.php"
+    method="POST">
+
+        <label
+        for="correo"
+        class="texto-vacio">
+
+            Correo:
+
+        </label>
+
         <br>
 
         <input
         type="email"
+        id="correo"
         name="correo"
         placeholder="correo@estudiantec.cr"
-        required>
+        required
+        class="campo-vacio">
 
         <br><br>
 
-        <label>Contraseña:</label>
+        <label
+        for="password"
+        class="texto-vacio">
+
+            Contraseña:
+
+        </label>
+
         <br>
 
         <input
         type="password"
+        id="password"
         name="password"
         minlength="8"
-        required>
+        required
+        class="campo-vacio">
 
         <br><br>
 
-        <input type="submit" value="Iniciar sesión">
+        <input
+        type="submit"
+        value="Iniciar sesión"
+        class="campo-vacio">
 
     </form>
 
     <br><br>
 
-    <a href="registrar.php">
+    <a
+    href="registrar.php"
+    class="texto-vacio">
+
         Crear cuenta
+
     </a>
+
+    <div class="barra-fondo-roja"></div>
+
+    <div class="barra-fondo-azul"></div>
 
 </body>
 </html>
